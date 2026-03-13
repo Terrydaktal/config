042bb744c4e52f89fadb800f73341605c3901711
Terrydaktal <9lewis9@gmail.com>
2026-03-13 16:39:18 +0000

    Tidy

--- config.fish
+++ config.fish
@@ -22,18 +22,15 @@ if status is-interactive
     alias la 'eza -lgAh --git --group-directories-first --hyperlink'
     alias pwd='printf "\e]8;;file://%s%s\a%s\e]8;;\a\n" (hostname) (string escape --style=url -- $PWD) "$PWD"'    
 
+    # Functions
     function expose; set -l target (realpath $argv[1]); set -l name (test (count $argv) -gt 1; and echo $argv[2]; or basename $argv[1]); ln -sf $target ~/.local/bin/$name; echo "Exposed $target as $name"; end; abbr -a expose expose
     function unexpose; set -l target "$HOME/.local/bin/"(basename $argv); if test -L $target; rm $target; echo "Unexposed $target"; else; echo "Error: $target is not a symlink in local bin"; end; end; abbr -a unexpose unexpose
-
-    # sudo wrapper: if "sudo rm ..." then use trash as root
     function sudo; test (count $argv) -ge 1; and test "$argv[1]" = "rm"; and command sudo /home/lewis/.local/bin/trash $argv[2..-1]; or command sudo $argv; end
-    
     function show_timestamp_after_command --on-event fish_postexec; set_color grey; echo (date "+[%d/%m/%y %H:%M:%S]") "$CMD_DURATION ms elapsed"; set_color normal; end
     function clipboard; if not isatty stdin; fish_clipboard_copy; else if count $argv > /dev/null; fish_clipboard_copy < $argv[1]; else; echo "Usage: cat file | clipboard  OR  clipboard filename"; end; end
-    
-    # Only bind Ctrl+Backspace in Fish, pass through to applications otherwise
     function smart_ctrl_backspace; set -l c (commandline); if string match -qr '.*\n.*' -- "$c"; commandline -f backward-kill-word; else if test -n "$c"; commandline -f backward-kill-word; else; commandline -i -- "\x17"; end; end
-   
+
+    # Binds
     bind \e\[1\;5A "set -l r (zoxide query -i); if test -n \"\$r\"; if string match -q '* *' \"\$r\"; commandline -i \"'\$r'\"; else; commandline -i \"\$r\"; end; end; commandline -f repaint"
     bind \b smart_ctrl_backspace
    
