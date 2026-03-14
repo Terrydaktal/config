a45956c23125dfc41b139321f996b8735a9aeb94
Terrydaktal <9lewis9@gmail.com>
2026-03-14 13:24:23 +0000

    Add keyboard shortcuts for terminal actions

---
+++ xfce4-terminal/accels.scm
@@ -0,0 +1,5 @@
+(gtk_accel_path "<Actions>/terminal-window/zoom-in" "")
+(gtk_accel_path "<Actions>/terminal-window/paste" "<Primary>v")
+(gtk_accel_path "<Actions>/terminal-window/copy" "<Primary>c")
+(gtk_accel_path "<Actions>/terminal-window/zoom-out" "")
+
