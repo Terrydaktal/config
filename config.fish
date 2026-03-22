1b5cb658e640038add2123da99e52cf354826630
Terrydaktal <9lewis9@gmail.com>
2026-03-22 02:58:34 +0000

    Update aliases and functions in config.fish
    move is now copy --move and tree is now our own tree script

--- config.fish
+++ config.fish
@@ -16,20 +16,19 @@ if status is-interactive
     test -d "$JAVA_HOME/bin"; and fish_add_path "$JAVA_HOME/bin"
 
     # Aliases
-    alias mv '/home/lewis/.local/bin/move'
+    alias mv '/home/lewis/.local/bin/copy --move'
     alias dust 'dust -d 1' #counts multiple hardlinks as one unless -s
     alias rm '/home/lewis/.local/bin/trash'
     alias cp '/home/lewis/.local/bin/copy'
-    alias tree 'tree -F -L 2 --dirsfirst --filelimit 20'
+    alias tree '/home/lewis/.local/bin/tree -F -a -L 2 -T 10 --hyperlinks'
     alias mkdir 'mkdir -p'
     alias pwd='printf "\e]8;;file://%s%s\a%s\e]8;;\a\n" (hostname) (string escape --style=url -- $PWD) "$PWD"'    
 
     # Functions
-    function ls; eza_wrapper eza --sort=type --group-directories-first -F --hyperlink -- $argv; end
+    function ls; eza_wrapper eza -a --sort=type --group-directories-first -F --hyperlink -- $argv; end
     function ll; set -l g --git; if test (count *) -lt 1000; and git rev-parse --is-inside-work-tree >/dev/null 2>&1; set -a g --git-repos; end; eza_wrapper eza -lghF $g --group-directories-first --sort=type --hyperlink -- $argv; end
     function la; set -l g --git; if test (count -A *) -lt 1000; and git rev-parse --is-inside-work-tree >/dev/null 2>&1; set -a g --git-repos; end; eza_wrapper eza -lgAhF $g --group-directories-first --sort=type --hyperlink -- $argv; end
     function eza_wrapper; set -l cmd $argv[1]; set -e argv[1]; set -l f; set -l i 1; while test $i -le (count $argv); if test "$argv[$i]" = "--sort"; set -l fd $argv[(math $i + 1)]; set -l or $argv[(math $i + 2)]; if test "$or" = asc; set -a f --sort=$fd; set i (math $i + 3); else if test "$or" = desc; set -a f --sort=$fd -r; set i (math $i + 3); else; set -a f --sort=$fd; set i (math $i + 2); end; else; set -a f "$argv[$i]"; set i (math $i + 1); end; end; command $cmd $f; end
-
     function expose; set -l target (realpath $argv[1]); set -l name (test (count $argv) -gt 1; and echo $argv[2]; or basename $argv[1]); ln -sf $target ~/.local/bin/$name; echo "Exposed $target as $name"; end; abbr -a expose expose
     function unexpose; set -l target "$HOME/.local/bin/"(basename $argv); if test -L $target; rm $target; echo "Unexposed $target"; else; echo "Error: $target is not a symlink in local bin"; end; end; abbr -a unexpose unexpose
     function sudo; test (count $argv) -ge 1; and test "$argv[1]" = "rm"; and command sudo /home/lewis/.local/bin/trash $argv[2..-1]; or command sudo $argv; end
